5934b3e07d7ea664959e8e5c15c5f197ba86a773
Alexandre Junior <alexandrejunior408@gmail.com>
2023-12-01 17:27:17 -0300

    feat: configure OData service on app

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,33 +3,31 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageBox, JSONModel) {
+  function (Controller, MessageBox, JSONModel, ODataModel) {
     "use strict";
 
     return Controller.extend("com.lab2dev.firstapp.controller.Home", {
       onInit: function () {
-        const products = [
-          { title: "Arroz", counter: 5 },
-          { title: "Feijão", counter: 8 },
-          { title: "Macarrão", counter: 10 },
-          { title: "Sal", counter: 2 },
-          { title: "Farinha de Trigo", counter: 9 },
-          { title: "Sabonete", counter: 11 },
-          { title: "Cafe", counter: 3 },
-          { title: "Oleo de Cozinha", counter: 3 },
-          { title: "Creme de Leite", counter: 4 },
-          { title: "Leite", counter: 6 },
-        ];
+        const oDataModel = new ODataModel("/northwind/northwind.svc/");
 
-        // Cria um modelo JSON com os produtos (JSONModel)
-        const oModel = new JSONModel(products);
+        oDataModel.read("/Products", {
+          success: (oProducts) => {
+            console.log(oProducts);
+            const products = oProducts.results;
 
-        // Define o modelo com o nome "products" e fazendo a ligação com a view (Aggregation Binding)
-        this.getView().setModel(oModel, "products");
+            console.log(products);
+            const oModel = new JSONModel(products);
+            this.getView().setModel(oModel, "products");
+          },
+          error: (oError) => {
+            MessageBox.error("Erro ao carregar os dados");
+          },
+        });
       },
       onPress: function (oEvent) {
         // Origem do evento
