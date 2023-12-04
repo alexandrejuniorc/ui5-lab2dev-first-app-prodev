ed9fa39d5047bcd33d044bce11e07c0343b0a7d9
Alexandre Junior <alexandrejunior408@gmail.com>
2023-12-04 12:04:58 -0300

    feat: create a filter to list

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,11 +3,13 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
     "com/lab2dev/firstapp/model/models",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageBox, models) {
+  function (Controller, MessageBox, models, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("com.lab2dev.firstapp.controller.Home", {
@@ -30,7 +32,9 @@ sap.ui.define(
         // Título do item
         const title = item.getTitle();
 
-        const i18n = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+        const i18n = this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle();
 
         // Mensagem a ser exibida
         const message = i18n.getText("dynamicItemClicked", [title]);
@@ -38,6 +42,21 @@ sap.ui.define(
         MessageBox.information(message, {
           title: "Informação do Item",
         });
+      },
+      onSearch: function (oEvent) {
+        // add filter for search
+        const aFilters = [];
+        const sQuery = oEvent.getSource().getValue();
+
+        if (sQuery && sQuery.length > 0) {
+            const filter = new Filter("ProductName", FilterOperator.Contains, sQuery);
+            aFilters.push(filter);
+        }
+
+        // update list binding
+        const oList = this.byId("list");
+        const oBinding = oList.getBinding("items");
+        oBinding.filter(aFilters);
       },
     });
   }
